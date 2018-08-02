624fb9881b95915cb1103eaa7c2f14cd289b168b
yqf3139 <yqf3139@gmail.com>
2018-08-02 13:03:23 +0800

    Update possibility

--- server/app-image-recognize.py
+++ server/app-image-recognize.py
@@ -8,14 +8,18 @@ def main():
     current_app.logger.info("Received request")
     body = request.get_data().decode("utf-8")
     category = 'cat'
+    possibility = 100
     name = 'none'
     try:
         payload = json.loads(body)['payload']
-        m = re.search(r'-(\w+)-', payload['Key'])
-        name = payload['Key'].split('/')[1]
         current_app.logger.info(payload["Key"])
+        name = payload['Key'].split('/')[1]
+        m = re.search(r'-(\w+)-', payload['Key'])
         if m:
             category = m.group(1)
+        m = re.search(r'_(\w+)_', payload['Key'])
+        if m:
+            possibility = int(m.group(1))
     except Exception as e:
         current_app.logger.error(e)
         pass
@@ -24,4 +28,6 @@ def main():
     return json.dumps({'err':'', 'payload': {
         'category': category,
         'name': name,
+        'possibility': possibility
     }})
+
